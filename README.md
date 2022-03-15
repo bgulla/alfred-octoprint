699fdbf95366bc9939da39ebe666241346a2e6ab
bgulla <hey@bgulla.dev>
2022-03-15 10:46:08 -0400

    adding example

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # alfred-octoprint
 retrieve octoprint api calls from MacOS Alfred
+
+in dev, do not use.
---
+++ app.py
@@ -0,0 +1,8 @@
+#!/usr/local/bin/python3
+
+import octorest
+import os
+
+OCTOPRINT_HOST=os.getenv(OCTOPRINT_HOST,'')
+OCTOPRINT_API_KEY=os.getenv(OCTOPRINT_API_KEY,'')
+
